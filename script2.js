6018cdc24409b1bd1102107c0611ca14b05412fc
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2020-09-14 19:25:39 -0300

    Acrescimo de consições de [erro]

--- script2.js
+++ script2.js
@@ -6,15 +6,22 @@ function contar(){
     var fim = Number.parseFloat(txfim.value);
     var txint = window.document.querySelector('input#txtint');
     var int = Number.parseFloat(txint.value);
-    if(ini < fim){
-        while(ini <= fim){
-            result.innerHTML += `&#9758 ${ini} `
-           ini = ini + int;
-    }
+    if(txini.value.length == 0 || txfim.value.length == 0 || txint.value.length == 0){
+        result.innerHTML = '[ERRO] Existem campos não preenchidos'
+    }else if( int == 0){
+        result.innerHTML = 'Por favor insira um valor diferente de "0" no campo Intervalo'
     }else{
-        while(ini >= fim){
-            result.innerHTML += `&#9758 ${ini} `
-           ini = ini - int;
+        if(ini < fim){
+            while(ini <= fim){
+                result.innerHTML += `${ini} \u{27A1} `;
+               ini = ini + int;
         }
+        }else{
+            while(ini >= fim){
+                result.innerHTML += `${ini} \u{27A1} `;
+               ini = ini - int;
+            }
+        }
+        result.innerHTML += `\u{2694}`;
     }
 }
